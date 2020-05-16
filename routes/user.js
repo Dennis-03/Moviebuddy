a244f11806965e1752b094ca848c4f584b99bf43
Dennisovich-03 <54091928+Dennisovich-03@users.noreply.github.com>
2020-05-17 00:00:29 +0530

    forgot password route

--- routes/user.js
+++ routes/user.js
@@ -6,13 +6,14 @@ const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API);
+const salt = bcrypt.genSaltSync(10);
 
 // Load User model
 const User = require("../models/users");
 const { forwardAuthenticated } = require("../config/auth");
 
 // Email Secret
-const EMAIL_SECRET = process.env.JWT_KEY;
+const JWT_KEY = process.env.JWT_KEY;
 const DOMAIN_URL = process.env.DOMAIN_URL;
 
 // Transporter
@@ -83,7 +84,7 @@ router.post("/signup", (req, res) => {
                   email: user.email,
                   name: user.name,
                 },
-                EMAIL_SECRET,
+                JWT_KEY,
                 {
                   expiresIn: "1h",
                 }
@@ -140,7 +141,7 @@ router.post("/signup", (req, res) => {
 
 router.get("/confirmation/:token", async (req, res) => {
   try {
-    const decoded = jwt.verify(req.params.token, EMAIL_SECRET);
+    const decoded = jwt.verify(req.params.token, JWT_KEY);
     // console.log(decoded);
     await User.findOneAndUpdate(
       { email: decoded.email },
@@ -189,8 +190,88 @@ router.get("/logout", (req, res) => {
 
 // list movies
 
-router.get("/profile", async (req, res) => {
+router.get("/profile", (req, res) => {
   res.render("user/profile", { user: req.user });
+});
+
+router.get("/forgot", (req, res) => {
+  res.render("user/forgot");
+});
+
+router.post("/forgot", (req, res) => {
+  const email = req.body.email;
+  User.findOne({ email }).then((user) => {
+    if (user != null) {
+      const reset = jwt.sign(
+        {
+          email: user.email,
+          name: user.name,
+        },
+        JWT_KEY,
+        {
+          expiresIn: "1d",
+        }
+      );
+      const url = `${DOMAIN_URL}/user/reset/${reset}`;
+
+      const msg = {
+        to: email,
+        from: process.env.GMAIL_USER,
+        subject: "Reset Password",
+        html: `<h1 style="color:blue;">Movie buddy</h1>
+        <p style="tesxt-align:justify;">Someone (hopefully you) has requested a password reset for your Movie buddy account.<br />
+        Follow the link below to set a new password:</p>
+        <a href="${url}"><strong>Reset Password</strong></a> <br /><br />
+        If you don't wish to reset your password, disregard this email and no action will be taken.`,
+      };
+
+      sgMail.send(msg).then(
+        () => {},
+        (error) => {
+          console.error(error);
+
+          if (error.response) {
+            console.error(error.response.body);
+          }
+        }
+      );
+    }
+  });
+  res.redirect("/user/login");
+});
+
+router.get("/reset/:token", (req, res) => {
+  res.render("user/reset", { token: req.params.token });
+});
+
+router.post("/reset", async (req, res) => {
+  const token = req.body.token;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+  console.log(token);
+
+  if (password != password2) {
+    req.flash("success_msg", "Passwords do not match");
+    res.send("/user/reset");
+  } else if (password.length < 6) {
+    req.flash("success_msg", "Minimum 6 character");
+    res.send("/user/reset");
+  } else {
+    try {
+      const decoded = jwt.verify(token.trim(), JWT_KEY);
+      const hash = bcrypt.hashSync(password, salt);
+      await User.findOneAndUpdate(
+        { email: decoded.email },
+        {
+          password: hash,
+        },
+        { new: true }
+      );
+    } catch (e) {
+      console.log(e);
+    }
+    res.redirect("/user/login");
+  }
 });
 
 module.exports = router;
